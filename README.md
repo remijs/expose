474c45a33a1e91a690c6e1367077e6f1e4fd6a6e
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-17 00:01:51 +0200

    module created

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # remi-expose
+
+A remi extension that adds the expose method to the target
+
+[![Dependency Status](https://david-dm.org/zkochan/remi-expose/status.svg?style=flat)](https://david-dm.org/zkochan/remi-expose)
+[![Build Status](https://travis-ci.org/zkochan/remi-expose.svg?branch=master)](https://travis-ci.org/zkochan/remi-expose)
+[![npm version](https://badge.fury.io/js/remi-expose.svg)](http://badge.fury.io/js/remi-expose)
+[![Coverage Status](https://coveralls.io/repos/zkochan/remi-expose/badge.svg?branch=master&service=github)](https://coveralls.io/github/zkochan/remi-expose?branch=master)
+
+
+## Installation
+
+```
+npm i remi-expose
+```
+
+
+## Usage
+
+Registering the extension
+
+```js
+const Remi = require('remi')
+const remiExpose = require('remi-expose')
+
+let remi = new Remi({
+  extensions: [remiExpose],
+})
+```
+
+Once the remi-expose extension is registered, the remi plugins can expose values.
+
+## app.expose(key, value)
+
+Used within a plugin to expose a property via app.plugins[name] where:
+
+* `key` - the key assigned (`server.plugins[name][key]`).
+* `value` - the value assigned.
+
+```js
+exports.register = function(app, opts) {
+  app.expose('util', () => console.log('something'))
+}
+```
+
+
+## server.expose(obj)
+
+Merges a shallow copy of an object into to the existing content of `server.plugins[name]` where:
+
+* `obj` - the object merged into the exposed properties container.
+
+```js
+exports.register = function(app, opts) {
+  app.expose({
+    util() {
+      console.log('something')
+    },
+  })
+}
+```
+
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,69 @@
+'use strict'
+const chai = require('chai')
+const expect = chai.expect
+
+const plugiator = require('plugiator')
+const Remi = require('remi')
+const expose = require('../')
+
+describe('plugin expose', function() {
+  let app
+  let remi
+
+  beforeEach(function() {
+    app = {}
+    remi = new Remi({
+      extensions: [expose],
+    })
+  })
+
+  it('should expose property', function() {
+    return remi.register(
+        app,
+        [plugiator.create('plugin', (app, opts) => app.expose('foo', 1))]
+      )
+      .then(() => expect(app.plugins.plugin.foo).to.eq(1))
+  })
+
+  it('should expose object', function() {
+    let plugin = plugiator.create('plugin', (app, options) => {
+      app.expose({
+        foo: 1,
+        bar: 3,
+      })
+    })
+
+    return remi.register(app, [plugin])
+      .then(() => {
+        expect(app.plugins.plugin.foo).to.eq(1)
+        expect(app.plugins.plugin.bar).to.eq(3)
+      })
+  })
+
+  it('should have a plugin namespace in plugins', function() {
+    let plugin = plugiator.create('foo-plugin', (app, options) => {
+      expect(app.plugins['foo-plugin']).to.be.not.undefined
+    })
+
+    return remi.register(app, [plugin])
+      .then(() => expect(app.plugins['foo-plugin']).to.be.not.undefined)
+  })
+
+  it('should share the plugin namespace through register invocations', function() {
+    let plugin = plugiator.create('foo-plugin', (app, options) => {
+      expect(app.plugins['foo-plugin']).to.be.not.undefined
+    })
+
+    return remi
+      .register(app, plugin, {})
+      .then(() => {
+        expect(app.plugins['foo-plugin']).to.be.not.undefined
+
+        return remi.register(app, [plugiator.noop()], {})
+      })
+      .then(() => {
+        expect(app.plugins).to.be.not.undefined
+        expect(app.plugins['foo-plugin']).to.be.not.undefined
+      })
+  })
+})

608cb3749345f29aa2331a2603064dc3b14e211a
Garth Braithwaite <garthdb@gmail.com>
2016-07-29 00:21:09 -0600

    Updated dependencies and tests for remi 2.x.x (#1)
    * updated remi dependency and tests
    
    * added nyc for test coverage

--- test/index.js
+++ test/index.js
@@ -1,33 +1,33 @@
-'use strict'
-const chai = require('chai')
-const expect = chai.expect
+import {expect} from 'chai'
+import chai from 'chai'
+import * as plugiator from 'plugiator'
+import remi from 'remi'
+import expose from '..'
 
-const plugiator = require('plugiator')
-const remi = require('remi')
-const expose = require('..')
-
-describe('plugin expose', function() {
+describe('plugin expose', () => {
   let app
   let registrator
 
-  beforeEach(function() {
+  beforeEach(() => {
     app = {}
     registrator = remi(app)
     registrator.hook(expose())
   })
 
-  it('should expose property', function() {
-    return registrator.register(
-        plugiator.create('plugin', (app, opts, next) => {
-          app.expose('foo', 1)
-          next()
-        })
-      )
+  it('should expose property', (done) => {
+    const plugin = plugiator.create('plugin', (app, opts, next) => {
+      app.expose('foo', 1)
+      next()
+    })
+
+    return registrator
+      .register({ register: plugin })
       .then(() => expect(app.plugins.plugin.foo).to.eq(1))
+      .then(() => done())
   })
 
-  it('should expose object', function() {
-    let plugin = plugiator.create('plugin', (app, options, next) => {
+  it('should expose object', (done) => {
+    const plugin = plugiator.create('plugin', (app, options, next) => {
       app.expose({
         foo: 1,
         bar: 3,
@@ -35,24 +35,28 @@ describe('plugin expose', function() {
       next()
     })
 
-    return registrator.register([plugin])
+    return registrator
+      .register({ register: plugin })
       .then(() => {
         expect(app.plugins.plugin.foo).to.eq(1)
         expect(app.plugins.plugin.bar).to.eq(3)
       })
+      .then(() => done())
   })
 
-  it('should have a plugin namespace in plugins', function() {
+  it('should have a plugin namespace in plugins', (done) => {
     const plugin = plugiator.create('foo-plugin', (app, options, next) => {
       expect(app.plugins.fooPlugin).to.be.not.undefined
       next()
     })
 
-    return registrator.register([plugin])
+    return registrator
+      .register({ register: plugin })
       .then(() => expect(app.plugins.fooPlugin).to.be.not.undefined)
+      .then(() => done())
   })
 
-  it('should not camel case the plugin namespace', function() {
+  it('should not camel case the plugin namespace', (done) => {
     const plugin = plugiator.create('foo-plugin', (app, options, next) => {
       expect(app.plugins['foo-plugin']).to.be.not.undefined
       next()
@@ -61,26 +65,28 @@ describe('plugin expose', function() {
     const registrator = remi(app)
     registrator.hook(expose({ camelCase: false }))
 
-    return registrator.register([plugin])
+    return registrator.register({ register: plugin })
       .then(() => expect(app.plugins['foo-plugin']).to.be.not.undefined)
+      .then(() => done())
   })
 
-  it('should share the plugin namespace through register invocations', function() {
+  it('should share the plugin namespace through register invocations', (done) => {
     const plugin = plugiator.create('foo-plugin', (app, options, next) => {
       expect(app.plugins.fooPlugin).to.be.not.undefined
       next()
     })
 
     return registrator
-      .register([plugin])
+      .register({ register: plugin })
       .then(() => {
         expect(app.plugins.fooPlugin).to.be.not.undefined
 
-        return registrator.register([plugiator.noop()])
+        return registrator.register({ register: plugiator.noop() })
       })
       .then(() => {
         expect(app.plugins).to.be.not.undefined
         expect(app.plugins.fooPlugin).to.be.not.undefined
       })
+      .then(() => done())
   })
 })
